f9da697d96e7f2323b6886ea34a84352f80d05ce
gilles20393 <gilles1996@outlook.com>
2023-08-06 05:51:33 +0200

    tv be refactored code

--- index.js
+++ index.js
@@ -3,35 +3,50 @@ const axios = require('axios');
 
 // Configuration for the channels you want to include
 const config = {
-    countries: ['BE',],
-    languages: ['nld',],
+    countries: ['BE'],
+    languages: ['nld'],
     excludeCategories: ['music', 'general', 'Religious', 'news', 'XXX'],
 };
 
+// Addon Manifest
 const addon = new addonBuilder({
     id: 'org.iptvaddon',
     name: 'IPTV Addon',
     version: '0.0.1',
-    description: 'Watch live tv from selected countries and languages',
+    description: 'Watch live TV from selected countries and languages',
     resources: ['catalog', 'meta', 'stream'],
-    types: ['channel'],
-    catalogs: [
-        {
-            type: 'channel',
-            id: 'SelectedCatalog',
-            name: 'Selected Channels',
-        },
-    ],
+    types: ['tv'],
+    catalogs: [{
+        type: 'tv',
+        id: 'iptv-channels',
+        name: 'IPTV',
+    }],
     idPrefixes: ['iptv-'],
 });
 
 // Convert channel to Stremio accepted Meta object
-const toMeta = (channel) => ({
+const toMeta = (channel, guideDetails) => ({
     id: `iptv-${channel.id}`,
     name: channel.name,
-    type: 'channel',
-    poster: channel.logo || null,
+    type: 'tv',
+    genres: channel.categories || null,
+    poster: guideDetails ? guideDetails.currentShowImage : channel.logo,
+    posterShape: 'square',
+    background: channel.logo || null,
+    logo: channel.logo || null,
+    description: guideDetails ? `Now Playing: ${guideDetails.nowPlaying}\nNext: ${guideDetails.next}` : null,
 });
+
+// Function to extract relevant details from guide information (replace with actual logic)
+const extractGuideDetails = (guide) => {
+    console.log(guide);
+    // Extract relevant details from guide information (update as needed)
+    return {
+        nowPlaying: 'Example Now Playing',
+        next: 'Example Next Show',
+        currentShowImage: 'https://example.com/show-image.png',
+    };
+};
 
 // Fetch Channels based on the configuration
 const getChannels = async () => {
@@ -40,26 +55,39 @@ const getChannels = async () => {
         config.countries.includes(channel.country) &&
         config.languages.some(lang => channel.languages.includes(lang)) &&
         !config.excludeCategories.some(cat => channel.categories.includes(cat))
-    ).map(toMeta);
+    ).map((channel) => toMeta(channel, null)); // Pass null for guideDetails
 };
 
 // Catalog Handler
 addon.defineCatalogHandler(async (args) => {
-    if (args.type === 'channel' && args.id === 'SelectedCatalog') {
+    if (args.type === 'tv' && args.id === 'iptv-channels') {
         const channels = await getChannels();
+        console.log('channels: ', channels);
         return { metas: channels };
     }
     return { metas: [] };
 });
 
+// Fetch Guide Info for the Channel
+const getGuideInfo = async (channelID) => {
+    const response = await axios.get('https://iptv-org.github.io/api/guides.json');
+    return response.data.find((guide) => guide.channel === channelID);
+};
+
 // Meta Handler
 addon.defineMetaHandler(async (args) => {
-    if (args.type === 'channel' && args.id.startsWith('iptv-')) {
+    if (args.type === 'tv' && args.id.startsWith('iptv-')) {
         const channelID = args.id.split('iptv-')[1];
         const response = await axios.get('https://iptv-org.github.io/api/channels.json');
         const channel = response.data.find((channel) => channel.id === channelID);
         if (channel) {
-            return { meta: toMeta(channel) };
+            // Fetch guide information
+            const guideInfo = await getGuideInfo(channelID);
+            // Extract relevant details from guide information
+            const details = extractGuideDetails(guideInfo);
+            // Modify the meta object based on the guide information
+            const meta = toMeta(channel, details);
+            return { meta };
         }
     }
     return { meta: {} };
@@ -67,11 +95,15 @@ addon.defineMetaHandler(async (args) => {
 
 // Stream Handler
 addon.defineStreamHandler(async (args) => {
-    if (args.type === 'channel' && args.id.startsWith('iptv-')) {
+    if (args.type === 'tv' && args.id.startsWith('iptv-')) {
         const channelID = args.id.split('iptv-')[1];
         const streamsResponse = await axios.get('https://iptv-org.github.io/api/streams.json');
         const stream = streamsResponse.data.find((stream) => stream.channel === channelID);
+        console.log('channelID: ', channelID);
+
         if (stream) {
+            console.log('stream.channel: ', stream.channel);
+            console.log('stream: ', stream);
             return {
                 streams: [
                     {
@@ -80,6 +112,8 @@ addon.defineStreamHandler(async (args) => {
                     },
                 ],
             };
+        } else {
+            console.log('No matching stream found for channelID:', channelID);
         }
     }
     return { streams: [] };
